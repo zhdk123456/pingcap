3a1833bf478c022a191eb0e6f4ee31de420f10d2
bob bae <poofyleek@gmail.com>
2014-10-23 23:49:27 -0400

    go ping

--- pingcap.go
+++ pingcap.go
@@ -1,16 +1,18 @@
 package pingcap
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
-	"github.com/poofyleek/glog"
 	"github.com/miekg/pcap"
+	"github.com/poofyleek/glog"
 	"io/ioutil"
 	"net"
 	"os"
-	"os/exec"
 	"reflect"
 	"strings"
 	"sync"
+	"time"
 )
 
 type ScanResult struct {
@@ -19,18 +21,12 @@ type ScanResult struct {
 	SrcVendor  string
 }
 
-type PingResult struct {
-	IPAddr string
-	Status string
-}
-
 type PingScanResult struct {
 	Type string
 	Scan ScanResult
-	Ping PingResult
 }
 
-func pingAll(CIDR string, ch chan *PingScanResult) []PingResult {
+func pingAll(CIDR string, ch chan *PingScanResult) {
 	ip, ipnet, err := net.ParseCIDR(CIDR)
 	if err != nil {
 		glog.Fatal(err)
@@ -41,36 +37,17 @@ func pingAll(CIDR string, ch chan *PingScanResult) []PingResult {
 		targets = append(targets, ip.String())
 	}
 	var wg sync.WaitGroup
-	var results []PingResult
 	for _, ip := range targets {
 		wg.Add(1)
 		go func(ipa string) {
-			o, err := exec.Command("ping", "-c", "1", "-W", "1", ipa).CombinedOutput()
-			out := string(o)
-			res := PingResult{}
-			res.IPAddr = ipa
-			if err != nil {
-				if strings.Index(out, "100% packet loss") > 0 {
-					res.Status = "absent"
-				}
-			} else {
-				if strings.Index(out, " 0% packet loss") > 0 {
-					res.Status = "present"
-				} else {
-					res.Status = "fuzzy"
-					glog.V(2).Infof("%s %v", out, err)
-				}
-			}
-			results = append(results, res)
+			ping(ipa)
 			psRes := PingScanResult{}
 			psRes.Type = "ping"
-			psRes.Ping = res
 			ch <- &psRes
 			wg.Done()
 		}(ip)
 	}
 	wg.Wait()
-	return results
 }
 
 // From Russ Cox
@@ -94,10 +71,8 @@ func PingScan(CIDR, OUIFile, dev string, ch chan *PingScanResult) error {
 	if err != nil {
 		return err
 	}
-	var res []PingResult
 	go func() {
-		res = pingAll(CIDR, ch)
-		glog.V(2).Infof("%v", res)
+		pingAll(CIDR, ch)
 	}()
 	ouiDB := make(map[string]string)
 	ouiFileExists := true
@@ -164,4 +139,166 @@ func PingScan(CIDR, OUIFile, dev string, ch chan *PingScanResult) error {
 	//not reached
 	glog.V(2).Infof("exiting pcap capture. %v", h.Geterror())
 	return nil
+}
+
+// icmpMessage represents an ICMP message.
+type icmpMessage struct {
+	Type     int             // type
+	Code     int             // code
+	Checksum int             // checksum
+	Body     icmpMessageBody // body
+}
+
+// icmpMessageBody represents an ICMP message body.
+type icmpMessageBody interface {
+	Len() int
+	Marshal() ([]byte, error)
+}
+
+type icmpEcho struct {
+	ID   int    // identifier
+	Seq  int    // sequence number
+	Data []byte // data
+}
+
+const (
+	icmpv4EchoRequest = 8
+	icmpv4EchoReply   = 0
+	icmpv6EchoRequest = 128
+	icmpv6EchoReply   = 129
+)
+
+func ping(ip string) error {
+	c, err := net.Dial("ip4:icmp", ip)
+	if err != nil {
+		panic(err)
+	}
+	glog.V(4).Infof("icmp to %s\n", ip)
+	c.SetDeadline(time.Now().Add(time.Second))
+	defer c.Close()
+	typ := icmpv4EchoRequest
+	xid := os.Getpid() & 0xffff
+	xseq := 1
+	wb, err := (&icmpMessage{Type: typ, Code: 0, Body: &icmpEcho{ID: xid, Seq: xseq, Data: bytes.Repeat([]byte("Go ping"), 10)}}).Marshal()
+	if err != nil {
+		return err
+	}
+	if _, err := c.Write(wb); err != nil {
+		return err
+	}
+	rb := make([]byte, 20+len(wb))
+	var m *icmpMessage
+	for {
+		if _, err := c.Read(rb); err != nil {
+			if strings.HasSuffix(err.Error(), "i/o timeout") {
+				return fmt.Errorf("timeout: %v", err)
+			} else {
+				return (err)
+			}
+		}
+		rb = ipv4Payload(rb)
+		if m, err = parseICMPMessage(rb); err != nil {
+			return (err)
+		}
+		switch m.Type {
+		case icmpv4EchoRequest:
+			continue
+		}
+		break
+	}
+	switch p := m.Body.(type) {
+	case *icmpEcho:
+		if p.ID != xid || p.Seq != xseq {
+			fmt.Printf("bad xid %d != %d seq %d != %d\n", p.ID, xid, p.Seq, xseq)
+			return fmt.Errorf("bad xid")
+		}
+	default:
+		return fmt.Errorf("bad type")
+	}
+	glog.V(2).Infof("got %v", m)
+	return nil
+}
+
+func ipv4Payload(b []byte) []byte {
+	if len(b) < 20 {
+		return b
+	}
+	hdrlen := int(b[0]&0xf) << 2
+	return b[hdrlen:]
+}
+
+func parseICMPEcho(b []byte) (*icmpEcho, error) {
+	bodylen := len(b)
+	p := &icmpEcho{ID: int(b[0])<<8 | int(b[1]), Seq: int(b[2])<<8 | int(b[3])}
+	if bodylen > 4 {
+		p.Data = make([]byte, bodylen-4)
+		copy(p.Data, b[4:])
+	}
+	return p, nil
+}
+
+func parseICMPMessage(b []byte) (*icmpMessage, error) {
+	msglen := len(b)
+	if msglen < 4 {
+		return nil, errors.New("message too short")
+	}
+	m := &icmpMessage{Type: int(b[0]), Code: int(b[1]), Checksum: int(b[2])<<8 | int(b[3])}
+	if msglen > 4 {
+		var err error
+		switch m.Type {
+		case icmpv4EchoRequest, icmpv4EchoReply, icmpv6EchoRequest, icmpv6EchoReply:
+			m.Body, err = parseICMPEcho(b[4:])
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return m, nil
+}
+
+func (p *icmpEcho) Len() int {
+	if p == nil {
+		return 0
+	}
+	return 4 + len(p.Data)
+}
+
+// Marshal returns the binary enconding of the ICMP echo request or
+// reply message body p.
+func (p *icmpEcho) Marshal() ([]byte, error) {
+	b := make([]byte, 4+len(p.Data))
+	b[0], b[1] = byte(p.ID>>8), byte(p.ID)
+	b[2], b[3] = byte(p.Seq>>8), byte(p.Seq)
+	copy(b[4:], p.Data)
+	return b, nil
+}
+
+func (m *icmpMessage) Marshal() ([]byte, error) {
+	b := []byte{byte(m.Type), byte(m.Code), 0, 0}
+	if m.Body != nil && m.Body.Len() != 0 {
+		mb, err := m.Body.Marshal()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, mb...)
+	}
+	switch m.Type {
+	case icmpv6EchoRequest, icmpv6EchoReply:
+		return b, nil
+	}
+	csumcv := len(b) - 1 // checksum coverage
+	s := uint32(0)
+	for i := 0; i < csumcv; i += 2 {
+		s += uint32(b[i+1])<<8 | uint32(b[i])
+	}
+	if csumcv&1 == 0 {
+		s += uint32(b[csumcv])
+	}
+	s = s>>16 + s&0xffff
+	s = s + s>>16
+	// Place checksum back in header; using ^= avoids the
+	// assumption the checksum bytes are zero.
+	b[2] ^= byte(^s)
+	b[3] ^= byte(^s >> 8)
+	return b, nil
 }
